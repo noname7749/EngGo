070c633f02816c8a62cc60369afa243737d684ad
hunghz123 <hunghz123@gmail.com>
2024-11-27 01:35:00 +0700

    Update MainActivity.kt

--- app/src/main/java/com/example/enggo/MainActivity.kt
+++ app/src/main/java/com/example/enggo/MainActivity.kt
@@ -7,14 +7,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.enggo.model.Flashcard
-import com.example.enggo.model.FlashcardFolder
-import com.example.enggo.ui.flashcard.FlashcardFolderView
 import com.example.enggo.ui.navigation.App
 import com.example.enggo.ui.navigation.rememberAppState
 import com.example.enggo.ui.theme.EngGoTheme
@@ -26,15 +22,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             EngGoTheme {
-                var t : FlashcardFolder = FlashcardFolder()
-                t.name = "Testing flashcard folder"
-                t.addFlashcard(Flashcard("Vehicle", "Thuộc về xe cộ (adj)"))
-                t.addFlashcard(Flashcard("vinicity", "Vùng lân cận, phụ cận"))
-                t.addFlashcard(Flashcard("cater", "Phục vụ, cung cấp (v)"))
-                t.addFlashcard(Flashcard("Pastoral Care", "Mục vụ (trách nhiệm chăm sóc và tư vấn được cung cấp bởi các nhà lãnh đạo tôn giáo)"))
-                Surface(modifier = Modifier.fillMaxSize()) {
-                    FlashcardFolderView(fcFolder = t)
-                }
+                val appState = rememberAppState(this);
+                App(appState);
             }
         }
         FirebaseApp.initializeApp(this)
